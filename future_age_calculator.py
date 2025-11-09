1581aaa2f3d452f2daa041cae056757929429031
Thalenta Nkala <nkalathalenta@gmail.com>
2025-11-09 22:28:25 +0200

    Fix future_age_calculator script to pass all checks

--- future_age_calculator.py
+++ future_age_calculator.py
@@ -1,10 +1,4 @@
-# future_age_calculator.py
-
-# Prompt the user for their current age
+cho '# future_age_calculator.py
 current_age = int(input("How old are you? "))
-
-# Calculate age in 2050
-age_in_2050 = current_age + 27  # 2050 - 2023 = 27
-
-# Print the result
-print(f"In 2050, you will be {age_in_2050} years old.")
+age_in_2050 = current_age + 27
+print(f"In 2050, you will be {age_in_2050} years old.")'
